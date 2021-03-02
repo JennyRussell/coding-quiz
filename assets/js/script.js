6328556849e00b7ac6f6cc5a8e36d4e785f39040
Jenny Russell <moosyoolka@gmail.com>
2021-03-01 18:19:35 -0800

    added outline comments to js

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+var startQuiz = $('.start-quiz-button');
+
+
+
+
+//press button to start quiz
+
+//start 1 min countdown on start-button click
+
+
+//on starting quiz change .quiz-header to display question
+
+
+//on starting quiz change .quiz-body to display list of options
+
+
+//on start quiz, hide start quiz button
+
+
+//quiz list options change bg color to lighter shade when hovering over it
+
+
+// on wrong answer, move on to the next question
+// and insert container with top margin and text that says "Wrong!" below list
+// wrong answer deducts ten seconds from timer countdown
+
+
+// on correct answer, move to next question
+// and insert container with top margin that says "Correct!" below
+
+
+// on finishing quiz before timer runs out
+// display "All done!" in header
+// Your score "score" in body
+// dynamically insert inpout box for Enter Initials and button submit
+// hover on submit button changes color to lighter
+
+
+// upon clicking submit, hide navigation
+// Display Highscores in header
+// display light purple colored body with numbered list for first entry
+// display two new buttons at the bottom: go back, and clear highscores
+// lighter color hover effects on buttons
+
+
+//Clicking clear highscores deletes body list item
+//clicking go back takes you to start of quiz
