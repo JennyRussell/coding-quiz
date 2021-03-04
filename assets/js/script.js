2306688d5d60deca004d45d92cd52905c598da3c
Jenny Russell <moosyoolka@gmail.com>
2021-03-03 17:51:49 -0800

    added style for wrong/correct validator div and hid it on timeout

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,16 @@ let question1 = "These are all Javascript data types except:";
 let answers1 = ["undefined", "number", "boolean", "float"];
 let answers2 = ["A", "V", "X", "floaDDt"];
 
-let secondsLeft = 60;
+let secondsLeft = 20;
 let answerIndex = 0;
 
 // timer countdwon function
 function startCountdown() {
     timer = setInterval(function() {
         secondsLeft--;
-        if (secondsLeft >= 0) {
-            timeDisplay.innerHTML = secondsLeft;
-        }
-        if (secondsLeft === 0) {
-
+        timeDisplay.innerHTML = secondsLeft;
+        if (secondsLeft <= 0) {
+            timeDisplay.innerHTML = "0";
             clearInterval(secondsLeft);
         }
     }, 1000);
@@ -94,19 +92,28 @@ function injectAnswerText(isAnswerCorrect) {
 
 
     if (isAnswerCorrect) {
-        document.getElementById("test-answer").innerHTML = "CORRECT";
+        document.getElementById("test-answer").innerHTML = "Correct!";
+        document.getElementById("test-answer").style.borderTop = "5px solid grey";
         //inject correct answer text
         // makeVariable = injectedNodeText;
     } else {
-        document.getElementById("test-answer").innerHTML = "INCORRECT";
+        document.getElementById("test-answer").innerHTML = "Wrong!";
+        document.getElementById("test-answer").style.borderTop = "5px solid grey";
+
+        if (secondsLeft > 10) {
+            secondsLeft = secondsLeft - 10;
+        } else {
+            secondsLeft = 0;
+        }
         //inject incorrect answer text
         // makeVariable = injectedNodeText;
     }
     setTimeout(() => {
         //clear incorrect answer text
-        // makeVariable.remove();
+
         document.getElementById("test-answer").innerHTML = "";
-    }, 3000);
+        document.getElementById("test-answer").style.borderTop = "";
+    }, 4000);
 }
 
 //returns true of
